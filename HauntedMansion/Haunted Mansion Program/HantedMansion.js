1170c0839471cb311cb0dc67c7fc0cd966a75308
Nolan <02Nolan.Loukes@gulllakecs.org>
2022-01-26 09:36:23 -0800

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/Haunted Mansion Program/HantedMansion.js
@@ -0,0 +1,6 @@
+/*
+Programer: Nolan Loukes
+Team Member: Kyle
+Date: 1.26.2022
+Program: we will be making a program to go through a Haunted Mansion Maze with certain challenges and obstacles in Java Script
+*/
