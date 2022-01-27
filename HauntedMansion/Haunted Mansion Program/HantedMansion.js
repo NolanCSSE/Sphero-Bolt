27c21add628a394ea5ed84c73b8c790665e50071
Nolan <02Nolan.Loukes@gulllakecs.org>
2022-01-27 10:00:47 -0800

    Roll forward and start - Kyle and Nolan

--- HauntedMansion/Haunted Mansion Program/HantedMansion.js
+++ HauntedMansion/Haunted Mansion Program/HantedMansion.js
@@ -4,3 +4,10 @@ Team Member: Kyle
 Date: 1.26.2022
 Program: we will be making a program to go through a Haunted Mansion Maze with certain challenges and obstacles in Java Script
 */
+async function startProgram() {
+	// Write code here
+	setHeading(270);
+	await speak('Start');
+	await roll ((getHeading() + 90), 75,2);
+}
+
