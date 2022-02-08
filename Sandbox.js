cf2d8a9c39b0a27b7b84785f18a0b848ff80f020
Nolan <02Nolan.Loukes@gulllakecs.org>
2022-02-08 09:18:55 -0800

    Blue light + 90 degrees - Kyle

--- Sandbox.js
+++ Sandbox.js
@@ -4,6 +4,11 @@ Date: 3.10.20
 Program: this is a sandbox where I am learning multiple things with the Sphero Bolt.
 */
 async function startProgram() {
-	
+	// Write code here
+	setHeading(270);
+	await speak('Start');
+	await roll ((getHeading() + 90), 75,2);
+	await roll ((getHeading() + 90), 72,2);
+	setMainLed ({ r: 0, g: 0, b: 255});
+	await roll((getHeading() + 90), 67, 2);
 }
-
